16cb6ea50844c9d0c412e7424c71a0bdb57b001d
Martin Arroyo <ma.arroyoq@duocuc.cl>
2024-10-15 09:58:26 -0300

    creacion esqueleto del login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  username!: string;
+  password!: string;
+
   constructor() { }
 
   ngOnInit() {
